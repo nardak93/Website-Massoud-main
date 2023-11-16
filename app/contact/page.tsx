c6d23eadfe3c3880b080d2ba73726d214d49407d
nardak93 <nardak1993@gmail.com>
2023-11-17 00:15:16 +0100

    first commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+// page.tsx in app/(default)/contact/
+
+import ContactForm from '@/app/(default)/contact/ContactForm.client';
+
+const ContactPage = () => {
+    return (
+        <div>
+            {/* Other page content */}
+            <ContactForm />
+        </div>
+    );
+};
+
+export default ContactPage;
