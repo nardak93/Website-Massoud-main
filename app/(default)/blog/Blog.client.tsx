903658c14181cacda121643e74098d22069fddb7
nardak93 <nardak1993@gmail.com>
2024-03-17 13:20:37 +0100

    message

--- app/(default)/blog/Blog.client.tsx
+++ app/(default)/blog/Blog.client.tsx
@@ -1,119 +1,181 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
+import Image from "next/image";
 
-import img1 from '@/public/images/img1.jpg';
-import img2 from '@/public/images/img2.jpg';
-import img3 from '@/public/images/img3.jpg';
+import FeatImage01 from "@/public/images/inspection.png";
+import FeatImage02 from "@/public/images/inspection02.png";
+import FeatImage03 from "@/public/images/Stars.png";
+import CMM from "@/public/images/CMM.jpg";
+import hardness from "@/public/images/Hardness test.jpg";
+import roughness from "@/public/images/photo_33_2023-11-27_16-16-54.jpg";
+import salt from "@/public/images/Salt contamination check.jpg";
 
-const BlogPage = () => {
-    const [expandedStoryId, setExpandedStoryId] = useState(null);
-  // Fictional stories data
-  const stories = [
-    {
-      id: 1,
-      title: 'Behind the Lens: A Look at Our 2022 Quality Inspection Success',
-      content: `Last year marked a milestone for our quality inspection team at [Your Company Name]. With precision at the heart of our mission, we dedicated ourselves to elevating the standards of our factory's production line, ensuring every product not only met but exceeded our customers' expectations.
-
-      Our journey of meticulous inspection began with the assembly of a diverse team of experts, each bringing a unique skill set to the table. United by a shared commitment to excellence, we navigated through the year with a clear vision — to ensure the integrity of our manufacturing process through rigorous quality checks.
-      
-      A pivotal moment captured in the accompanying photograph illustrates the crux of our operation — the microscopic analysis of product samples. The focus and attention to detail personified by our specialist, as seen in the image, reflect the granular level of scrutiny we applied throughout our inspections.
-      
-      Utilizing advanced equipment, including the high-powered microscope featured in our story, we dissected every facet of our products. From material durability to the perfection of the finish, no aspect was too small for our evaluation. The results were nothing short of transformative. By identifying and addressing even the most minute irregularities, we not only improved the quality of our products but also streamlined our manufacturing process, reducing waste and increasing efficiency.
-      
-      Our 2022 quality inspection initiative wasn't just about maintaining standards — it was about setting new benchmarks for quality. As we share this glimpse into our work, we celebrate the unwavering dedication of our team and the seamless collaboration that led to our success. We look forward to continuing this journey, pushing the boundaries of quality, and upholding the trust that our customers place in us.`,
-      image: img1,
-      author: 'John Doe',
-      date: 'January 1, 2023',
-    },
-    {
-      id: 2,
-      title: 'Mysteries of the Ocean',
-      content: `Last year marked a milestone for our quality inspection team at [Your Company Name]. With precision at the heart of our mission, we dedicated ourselves to elevating the standards of our factory's production line, ensuring every product not only met but exceeded our customers' expectations.
-
-      Our journey of meticulous inspection began with the assembly of a diverse team of experts, each bringing a unique skill set to the table. United by a shared commitment to excellence, we navigated through the year with a clear vision — to ensure the integrity of our manufacturing process through rigorous quality checks.
-      
-      A pivotal moment captured in the accompanying photograph illustrates the crux of our operation — the microscopic analysis of product samples. The focus and attention to detail personified by our specialist, as seen in the image, reflect the granular level of scrutiny we applied throughout our inspections.
-      
-      Utilizing advanced equipment, including the high-powered microscope featured in our story, we dissected every facet of our products. From material durability to the perfection of the finish, no aspect was too small for our evaluation. The results were nothing short of transformative. By identifying and addressing even the most minute irregularities, we not only improved the quality of our products but also streamlined our manufacturing process, reducing waste and increasing efficiency.
-      
-      Our 2022 quality inspection initiative wasn't just about maintaining standards — it was about setting new benchmarks for quality. As we share this glimpse into our work, we celebrate the unwavering dedication of our team and the seamless collaboration that led to our success. We look forward to continuing this journey, pushing the boundaries of quality, and upholding the trust that our customers place in us.`,
-      image: img2,
-      author: 'Jane Smith',
-      date: 'February 14, 2023',
-    },
-    {
-      id: 3,
-      title: 'Stars Above',
-      content: `Last year marked a milestone for our quality inspection team at [Your Company Name]. With precision at the heart of our mission, we dedicated ourselves to elevating the standards of our factory's production line, ensuring every product not only met but exceeded our customers' expectations.
-
-      Our journey of meticulous inspection began with the assembly of a diverse team of experts, each bringing a unique skill set to the table. United by a shared commitment to excellence, we navigated through the year with a clear vision — to ensure the integrity of our manufacturing process through rigorous quality checks.
-      
-      A pivotal moment captured in the accompanying photograph illustrates the crux of our operation — the microscopic analysis of product samples. The focus and attention to detail personified by our specialist, as seen in the image, reflect the granular level of scrutiny we applied throughout our inspections.
-      
-      Utilizing advanced equipment, including the high-powered microscope featured in our story, we dissected every facet of our products. From material durability to the perfection of the finish, no aspect was too small for our evaluation. The results were nothing short of transformative. By identifying and addressing even the most minute irregularities, we not only improved the quality of our products but also streamlined our manufacturing process, reducing waste and increasing efficiency.
-      
-      Our 2022 quality inspection initiative wasn't just about maintaining standards — it was about setting new benchmarks for quality. As we share this glimpse into our work, we celebrate the unwavering dedication of our team and the seamless collaboration that led to our success. We look forward to continuing this journey, pushing the boundaries of quality, and upholding the trust that our customers place in us.`,
-      image: img3,
-      author: 'Alex Johnson',
-      date: 'March 29, 2023',
-    },
-
-  ];
-
-  const toggleStory = (id) => {
-    if (expandedStoryId === id) {
-        setExpandedStoryId(null); // Close the expanded story
-    } else {
-        setExpandedStoryId(id); // Expand the new story
-    }
-};
-
-return (
+export default function Zigzag() {
+  return (
     <section>
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <h1 className="h2 mb-4 mt-28">
+              EXPLORING PRECISION: YOUR GUIDE TO QUALITY INSPECTION
+            </h1>
+            <p className="text-xl text-gray-400">
+              At Inspection with Us, we specialize in comprehensive quality
+              inspection services, including mechanical, dimensional, painting,
+              and coating assessments, ensuring your products meet the highest
+              standards. With our expertise, your manufacturing processes are
+              optimized for precision and excellence. Trust Inspection with Us
+              to elevate your quality control to the next level.
+            </p>
+          </div>
 
-            {/* SVG background from Hero */}
-            <div className="absolute left-0 bottom-0 -ml-20 hidden lg:block pointer-events-none" aria-hidden="true">
-                {/* SVG code goes here */}
+          {/* Items */}
+          <div className="grid gap-20">
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center mt-28">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={CMM}
+                  width={840}
+                  height={405}
+                  alt="Features 01"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <h3 className="h3 mb-3">
+                    {" "}
+                    Unveiling the Role of CMM in Quality Inspection
+                  </h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    In this captivating snapshot, we witness the essence of
+                    precision encapsulated within the realm of quality
+                    inspection. Behold the Coordinate Measuring Machine (CMM), a
+                    marvel of engineering sophistication. As a seasoned quality
+                    inspector, I stand beside this technological masterpiece,
+                    its probing system delicately poised to capture the
+                    intricacies of a meticulously crafted component. With each
+                    precise movement, the CMM meticulously maps the dimensional
+                    accuracy and geometric intricacies, ensuring flawless
+                    adherence to stringent quality standards. This image
+                    epitomizes the pivotal role of CMMs in guaranteeing the
+                    excellence of manufactured products across diverse
+                    industries, from automotive to aerospace, fostering a
+                    culture of uncompromising quality and customer satisfaction.
+                  </p>
+                </div>
+              </div>
             </div>
 
-            {/* Content */}
-            <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
-                <div className="text-center mb-12">
-                    <h1 className="text-3xl font-semibold text-white-800">Our Blog</h1>
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={hardness}
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <h3 className="h3 mb-3">Sectors</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    We have a multidisciplinary team formed by well-educated and
+                    experienced engineers and technicians whom are located
+                    throughout Europe, enabling us to act as your independent
+                    eyes and ears, wherever your suppliers are based in. This
+                    extended experience in the mentioned fields shows our team
+                    experience and the quality of their service, which we were
+                    able to gain customer trust and satisfaction.
+                  </p>
                 </div>
-
-                {/* Blog stories */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {stories.map(story => (
-                        <article key={story.id} className="bg-black overflow-hidden shadow rounded-lg transition-all duration-500 ease-in-out">
-                            <div className="relative overflow-hidden pb-60">
-                                <Image src={story.image} alt={story.title} layout="fill" objectFit="cover" className="transition duration-500 ease-in-out transform hover:scale-105" />
-                            </div>
-                            <div className="p-6">
-                                <h2 className="text-xl font-bold text-white-900 mb-2 cursor-pointer" onClick={() => toggleStory(story.id)}>
-                                    {story.title}
-                                </h2>
-                                <p className={`text-base text-white-700 transition-all duration-500 ease-in-out ${expandedStoryId === story.id ? 'block' : 'line-clamp-3'}`}>
-                                    {story.content}
-                                </p>
-                                <footer className="text-gray-500">
-                                    <small>By {story.author} on {story.date}</small>
-                                </footer>
-                                <button
-                                    className="text-sm text-blue-500 hover:text-blue-600 mt-2"
-                                    onClick={() => toggleStory(story.id)}
-                                >
-                                    {expandedStoryId === story.id ? 'Read Less' : 'Read More'}
-                                </button>
-                            </div>
-                        </article>
-                    ))}
-                </div>
+              </div>
             </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={roughness}
+                  width={540}
+                  height={405}
+                  alt="Features 03"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={salt}
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <h3 className="h3 mb-3">Sectors</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    We have a multidisciplinary team formed by well-educated and
+                    experienced engineers and technicians whom are located
+                    throughout Europe, enabling us to act as your independent
+                    eyes and ears, wherever your suppliers are based in. This
+                    extended experience in the mentioned fields shows our team
+                    experience and the quality of their service, which we were
+                    able to gain customer trust and satisfaction.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
-);
-};
-
-export default BlogPage;
+  );
+}
